f6d53d2104a1a9de4d734f00e91536d1deec1e4d
Matt Gross <mattkgross@gmail.com>
2023-06-27 16:58:19 +0000

    disable equipment selection

--- src/config/feature_switches.py
+++ src/config/feature_switches.py
@@ -5,4 +5,4 @@ RUN_LAST_ACTIVITY_HIDE_HR: bool = True
 # If your latest activity has no description, fill it with a random quote.
 RUN_LAST_ACTIVITY_ADD_QUOTE: bool = True
 # If you specified equipment defaults for activity types, updates your last activity's data accordingly.
-RUN_LAST_ACTIVITY_EQUIPMENT: bool = True
+RUN_LAST_ACTIVITY_EQUIPMENT: bool = False # Strava just added this feature... finally.
